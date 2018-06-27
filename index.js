86462941145830afb55787650a5a75ca76500a52
Vanessa Sheldon <vsheldon@me.com>
2018-06-27 16:25:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "YES INDEED!"
   } else if (string.toLowerCase() == string) {
     return "I can't hear you!" 
-  } else if (string == "I love you, Grandma") {
+  } else if (string == "I love you, Grandma.") {
     return "I love you, too."
   }
 }
