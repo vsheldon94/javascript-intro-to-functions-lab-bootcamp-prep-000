602de90672964fe5bc35a58a14727b7118fa22ef
Vanessa Sheldon <vsheldon@me.com>
2018-06-27 16:20:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string.toUpperCase() == string {
+  if (string.toUpperCase() == string) {
     return "YES INDEED!"
   } else {
     return "I can't hear you"
